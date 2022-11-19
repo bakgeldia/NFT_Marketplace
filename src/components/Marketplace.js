ecb38a404dd68d270528c0619ad0db76f21fee8c
Bakgeldi Alkhabay <74294553+bakgeldia@users.noreply.github.com>
2022-11-19 03:08:11 +0100

    Add files via upload

---
+++ src/components/Marketplace.js
@@ -0,0 +1,92 @@
+import Navbar from "./Navbar";
+import NFTTile from "./NFTTile";
+import MarketplaceJSON from "../Marketplace.json";
+import axios from "axios";
+import { useState } from "react";
+
+export default function Marketplace() {
+const sampleData = [
+    {
+        "name": "NFT#1",
+        "description": "First NFT",
+        "website":"http://axieinfinity.io",
+        "image":"https://cdn.forbes.ru/forbes-static/750x422/new/2021/12/75f-61c1f76ccde7d.jpg",
+        "price":"0.03ETH",
+        "currentlySelling":"True",
+        "address":"0xe81Bf5A757CB4f7F82a2F23b1e59bE45c33c5b13",
+    },
+    {
+        "name": "NFT#2",
+        "description": "Second NFT",
+        "website":"http://axieinfinity.io",
+        "image":"https://thumbor.forbes.com/thumbor/fit-in/x/https://www.forbes.com/advisor/in/wp-content/uploads/2022/03/monkey-g412399084_1280.jpg",
+        "price":"0.03ETH",
+        "currentlySelling":"True",
+        "address":"0xe81Bf5A757C4f7F82a2F23b1e59bE45c33c5b13",
+    },
+    {
+        "name": "NFT#3",
+        "description": "Third NFT",
+        "website":"http://axieinfinity.io",
+        "image":"https://www.spot.uz/media/img/2022/04/pjXQH916492512110620_l.jpg",
+        "price":"0.03ETH",
+        "currentlySelling":"True",
+        "address":"0xe81Bf5A757C4f7F82a2F23b1e59bE45c33c5b13",
+    },
+];
+const [data, updateData] = useState(sampleData);
+const [dataFetched, updateFetched] = useState(false);
+
+async function getAllNFTs() {
+    const ethers = require("ethers");
+    //After adding your Hardhat network to your metamask, this code will get providers and signers
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const signer = provider.getSigner();
+    //Pull the deployed contract instance
+    let contract = new ethers.Contract(MarketplaceJSON.address, MarketplaceJSON.abi, signer)
+    //create an NFT Token
+    let transaction = await contract.getAllNFTs()
+
+    //Fetch all the details of every NFT from the contract and display
+    const items = await Promise.all(transaction.map(async i => {
+        const tokenURI = await contract.tokenURI(i.tokenId);
+        let meta = await axios.get(tokenURI);
+        meta = meta.data;
+
+        let price = ethers.utils.formatUnits(i.price.toString(), 'ether');
+        let item = {
+            price,
+            tokenId: i.tokenId.toNumber(),
+            seller: i.seller,
+            owner: i.owner,
+            image: meta.image,
+            name: meta.name,
+            description: meta.description,
+        }
+        return item;
+    }))
+
+    updateFetched(true);
+    updateData(items);
+}
+
+if(!dataFetched)
+    getAllNFTs();
+
+return (
+    <div>
+        <Navbar></Navbar>
+        <div className="flex flex-col place-items-center mt-20">
+            <div className="md:text-xl font-bold text-white">
+                Top NFTs
+            </div>
+            <div className="flex mt-5 justify-between flex-wrap max-w-screen-xl text-center">
+                {data.map((value, index) => {
+                    return <NFTTile data={value} key={index}></NFTTile>;
+                })}
+            </div>
+        </div>            
+    </div>
+);
+
+}
