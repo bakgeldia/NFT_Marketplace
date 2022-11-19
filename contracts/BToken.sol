2be6c67677e21fd48ca58e358935ebc4ab2134e2
Bakgeldi Alkhabay <74294553+bakgeldia@users.noreply.github.com>
2022-11-19 03:35:42 +0100

    Update BToken.sol

--- contracts/BToken.sol
+++ contracts/BToken.sol
@@ -2,20 +2,16 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
 
-contract BToken is ERC20 {
-    address public admin;
-    constructor() ERC20('BToken', 'BTN') {
-        _mint(msg.sender, 10000 * 10 ** 18);
-        admin = msg.sender;
+contract BToken is ERC20, ERC20Burnable, Ownable, ERC20Permit {
+    constructor() ERC20("BToken", "BTN") ERC20Permit("BToken") {
+        _mint(msg.sender, 10000 * 10 ** decimals());
     }
 
-    function mint(address to, uint amount) external {
-        require(msg.sender == admin, 'only admin');
+    function mint(address to, uint256 amount) public onlyOwner {
         _mint(to, amount);
-    }
-
-    function burn(uint amount) external {
-        _burn(msg.sender, amount);
     }
 }
